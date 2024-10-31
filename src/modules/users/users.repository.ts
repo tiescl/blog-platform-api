fc591b93b6e3d7b47c57e245a7ccb5e8f2a99abd
tiescl <anvar.madvaliev@icloud.com>
2024-10-31 15:17:31 +0500

    feat: implement [PATCH /users/profile] and [PATCH /users/:userId] routes

--- src/modules/users/users.repository.ts
+++ src/modules/users/users.repository.ts
@@ -2,6 +2,7 @@ import { db } from "database/data-source";
 import { User } from "shared/entities";
 import { DatabaseError, NotFoundError } from "shared/errors";
 import { UsersCreateDto } from "./users.types";
+import { UsersUpdateDto } from "./dto/users-update.dto";
 
 export class UsersRepository {
     static async createUser(user: UsersCreateDto): Promise<User> {
@@ -23,6 +24,33 @@ export class UsersRepository {
         }
 
         return result[0];
+    }
+
+    static async updateUser(
+        userId: string,
+        user: UsersUpdateDto
+    ): Promise<User> {
+        const values = Object.values(user);
+        const columns = Object.keys(user);
+        for (let i = 0; i < columns.length; ++i) {
+            columns[i] = `${columns[i]} = $${i + 1}`;
+        }
+
+        const result = await db.manager.query(
+            `
+            UPDATE users
+            SET ${columns.join(", ")}
+            WHERE id = $${values.length + 1}
+            RETURNING *
+            `,
+            [...values, userId]
+        );
+
+        if (!result[0] || !result[0][0]) {
+            throw new DatabaseError("Errors connecting to the database");
+        }
+
+        return result[0][0];
     }
 
     static async getUser(
